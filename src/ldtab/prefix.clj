ac26306d9b64f329cdc9a3b3d18547b81f64e75a
chris <chris.kind.man@gmail.com>
2021-11-06 23:31:47 +0000

    Add prefix loading

---
+++ src/ldtab/prefix.clj
@@ -0,0 +1,36 @@
+(ns ldtab.prefix
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [clojure.java.jdbc :as jdbc])
+  (:gen-class)) 
+
+(defn parse-tsv
+  [input]
+  (with-open [reader (io/reader input)]
+  (doall
+    (csv/read-csv reader :separator \tab)))) 
+
+(defn load-db
+  [path]
+  {:classname "org.sqlite.JDBC"
+  :subprotocol "sqlite"
+  :subname path})
+
+(defn insert-prefix
+  [db prefix base]
+  (jdbc/insert! db :prefix {:prefix prefix :base base}))
+
+(defn insert-prefixes
+ [prefixes-path db-path]
+ (let [db (load-db db-path)
+       ;rest: parse prefixes and drop first row "prefix base"
+       prefixes (rest (parse-tsv prefixes-path))]
+   (doseq [p prefixes] 
+     (insert-prefix db (first p) (second p)))))
+
+;manual testing
+(defn -main [& args]
+  (def db-spec (load-db (second args)))
+  (println (jdbc/query db-spec ["SELECT * FROM prefix"]))
+  (insert-prefixes (first args) (second args))
+  (println (jdbc/query db-spec ["SELECT * FROM prefix"])))
