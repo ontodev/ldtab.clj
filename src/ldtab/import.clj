cd50149e9d92d1d1dc9540cd85cc26533d1e9242
chris <chris.kind.man@gmail.com>
2022-01-04 11:46:51 +0000

    Finish first prototype for importing RDF graphs

--- src/ldtab/import.clj
+++ src/ldtab/import.clj
@@ -18,31 +18,33 @@
 (defn insert-triple
   [db transaction graph subject predicate object datatype annotation] 
   (jdbc/insert! db :statement {:assertion transaction
-                               :retraction 0
+                               :retraction 0 ;hard-coded value
                                :graph graph
                                :subject subject
                                :predicate predicate 
                                :object object
-                               :datatype "no datatype"
-                               :annotation "no anntation"}))
+                               :datatype datatype
+                               :annotation annotation})) 
+(defn prepere-insert
+  [transaction graph subject predicate object datatype annotation] 
+  {:assertion transaction
+   :retraction 0 ;hard-coded value
+   :graph graph
+   :subject subject
+   :predicate predicate 
+   :object object
+   :datatype datatype
+   :annotation annotation})
 
-
-(defn insert-thin-triple
-  [db transaction graph thin-triple annotation] ;NB: this is given in Jena
-  (let [subject  (.getSubject thin-triple)
-        predicate (.getPredicate thin-triple)
-        object (.getObject thin-triple)]
-    (if (.isLiteral object)
-      (let [literal (.getLiteral object)
-            lexicalForm (.getLexicalForm literal)
-            languageTag (.language literal)
-            datatype (.getLiteralDatatypeURI object)]
-        (if (not (= languageTag ""));NB: Jena introduces data types for everything 
-          (str db " " transaction " " graph " " subject " " predicate " "  lexicalForm " " languageTag " " annotation)
-          (str db " " transaction " " graph " " subject " " predicate " "  lexicalForm " " datatype " " annotation)))
-      (str subject " " predicate " " object))))
-;TODO ;(insert-triple db transaction graph subject predicate lexicalForm (str languageTag datatype) annotation)
-
+(defn multi-insert
+  [json-triples db transaction graph]
+  (jdbc/insert-multi! db :statement (map #(prepere-insert transaction
+                                                          graph
+                                                          (get % "subject")
+                                                          (get % "predicate")
+                                                          (get % "object")
+                                                          (get % "datatype")
+                                                          (get % "annotation")) json-triples)))
 
 (defn annotations-for-stated-triples
   [annotations thin-triples thick-triples]
@@ -56,27 +58,39 @@
   (filter #(not (or
                   (contains? thin-triples (dissoc % "annotation"))
                   (contains? thick-triples %)))
-          annotations))
+          annotations)) 
+
+(defn remove-from-thin-backlog 
+  [backlog annotations]
+  (let [anno (map #(dissoc % "annotation") annotations)
+        anno-set (set anno)]
+    (into [] (map #(remove (fn [x] (contains? anno-set x)) %) backlog))))
+
+(defn remove-from-thick-backlog 
+  [backlog annotations]
+  (let [anno-set (set annotations)]
+    (into [] (map #(remove (fn [x] (contains? anno-set x)) %) backlog))))
 
 (defn import-rdf
-  [rdf-path db-path]
+  [rdf-path db-path graph]
   (let [db (load-db db-path)
         is (new FileInputStream rdf-path)
         it (RDFDataMgr/createIteratorTriples is Lang/RDFXML "base")
         windowsize 50]
     ;TODO refactor this into parsing.clj? or translation.clj since this translates RDF into thick-triples
     (loop [backlog '()
-           thin-backlog [0 0 0]
-           thick-backlog [0 0 0]
-           unstated-annotation-backlog (hash-set)]
+           thin-backlog [nil nil nil] 
+           thick-backlog [nil nil nil]
+           unstated-annotation-backlog (hash-set)
+           transaction 1]
       (when (.hasNext it) 
         (let [[thin kept thick] (parse/parse-window it windowsize backlog)
 
-            thin-rdf (map parse/jena-triple-2-string thin)
-            thick-rdf (map #(map parse/jena-triple-2-string %) thick)
+            thin-rdf-string (map parse/jena-triple-2-string thin)
+            thick-rdf-string (map #(map parse/jena-triple-2-string %) thick)
 
-            thin-json (thin2thick/thin-2-thick thin-rdf)
-            thick-json (map #(first (thin2thick/thin-2-thick %)) thick-rdf)
+            thin-json (thin2thick/thin-2-thick thin-rdf-string)
+            thick-json (map #(first (thin2thick/thin-2-thick %)) thick-rdf-string)
 
             annotation-json (filter #(contains? % "annotation") thick-json) 
             thin-backlog-flat (set (flatten thin-backlog))
@@ -85,6 +99,7 @@
             stated-annotation-json (annotations-for-stated-triples annotation-json
                                                                    thin-backlog-flat 
                                                                    thick-backlog-flat)
+
             unstated-annotation-json (annotations-for-non-stated-triples annotation-json
                                                                          thin-backlog-flat
                                                                          thick-backlog-flat)
@@ -100,13 +115,26 @@
                                                          (set unstated-annotation-json))
             ;remove found annotations
             updated-unstated-annotation-backlog (s/difference updated-unstated-annotation-backlog
-                                                              (set stated-annotation-backlog))]
+                                                              (set stated-annotation-backlog))
 
-          (run! println unstated-annotation-backlog)
+            annotations (concat annotation-json stated-annotation-backlog)
+            [thin1 thin2 thin3] (remove-from-thin-backlog thin-backlog annotations)
+            [thick1 thick2 thick3] (remove-from-thin-backlog thick-backlog annotations)]
+
+          ;insert into database
+          ;TODO update transaction 
+          (when thin3
+            (multi-insert thin3 db transaction graph))
+          (when thick3
+            (multi-insert thick3 db transaction graph))
+          (when annotations
+            (multi-insert annotations db transaction graph))
+
           (recur kept
-                 [thin-json (first thin-backlog) (second thin-backlog)]
-                 [thick-json (first thick-backlog) (second thick-backlog)]
-                 updated-unstated-annotation-backlog))))))
+                 [thin-json thin1 thin2] 
+                 [thick-json thick1 thick2]
+                 updated-unstated-annotation-backlog
+                 transaction))))))
 
 
 
@@ -114,6 +142,23 @@
   "Currently only used for manual testing."
   [& args] 
 
-  (import-rdf (first args) (second args)) 
+  ;(let [rdf-path (first args)
+  ;      is (new FileInputStream rdf-path)
+  ;      it (RDFDataMgr/createIteratorTriples is Lang/RDFXML "base")
+  ;      windowsize 50]
+
+  ;  (loop [backlog '()] 
+  ;    (when (.hasNext it) 
+  ;      (let [[thin kept thick] (parse/parse-window it windowsize backlog) 
+  ;             thick-rdf-string (map #(map parse/jena-triple-2-string %) thick)
+  ;             thick-json (map #(first (thin2thick/thin-2-thick %)) thick-rdf-string)]
+  ;        (run! println thick-json)
+  ;        ;(run! println (thin2thick/thin-2-thick (map parse/jena-triple-2-string thin)))
+  ;        ;(run! println (thin2thick/thin-2-thick (map #(map parse/jena-triple-2-string %) thick)))
+  ;        ;(thin2thick/thin-2-thick (map parse/jena-triple-2-string thin))
+  ;        (recur kept)))))
+          
+
+  (time (import-rdf (first args) (second args) "graph"))
   
   ) 
