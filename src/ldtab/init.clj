10443e35afb527803c6133a6e4a3a48340c1dcec
chris <chris.kind.man@gmail.com>
2022-02-26 20:58:22 +0000

    Resolve issue #1

--- src/ldtab/init.clj
+++ src/ldtab/init.clj
@@ -2,52 +2,38 @@
   (:require [clojure.java.jdbc :as jdbc]))
 
 (defn create-database
-  "Initializes a SQLite database by creating three tables:
-  1. ldtab: [key, value] pairs for tracking metadata
-  2. prefix: [prefix, base] for tracking prefixes
-  3. statement: [assertion, retraction, graph, subject, predicate, object, datatype, annotation] for statements (thick triples)" 
+  "Creates an SQLite file with three tables:
+  1. 'ldtab' with columns: [key, value],
+  2. 'prefix' with columns: [prefix, base],
+  3. 'statement' with columns: [assertion, retraction, graph, subject, predicate, object, datatype, annotation]." 
   [dbname] 
-  (def db-spec
-    {:dbtype "sqlite"
-     :dbname (str dbname) 
-     :user "myaccount"
-     :password "secret"})
+  (let [db-spec {:dbtype "sqlite"
+                 :dbname (str dbname) 
+                 :user "myaccount"
+                 :password "secret"}
 
-  (def metadata-table-ddl
-  (jdbc/create-table-ddl :ldtab
-                         [[:key "TEXT" "PRIMARY KEY"]
-                          [:value "TEXT"]]))
+        metadata-table-ddl (jdbc/create-table-ddl :ldtab
+                                                  [[:key "TEXT" "PRIMARY KEY"]
+                                                   [:value "TEXT"]])
 
-  (def prefix-table-ddl
-  (jdbc/create-table-ddl :prefix
-                         [[:prefix "TEXT" "PRIMARY KEY"]
-                          [:base "TEXT" "NOT NULL"]]))
+        prefix-table-ddl (jdbc/create-table-ddl :prefix
+                                                [[:prefix "TEXT" "PRIMARY KEY"]
+                                                 [:base "TEXT" "NOT NULL"]])
 
-  (def statement-table-ddl
-  (jdbc/create-table-ddl :statement
-                         [[:assertion :int "NOT NULL"] 
-                          [:retraction :int "NOT NULL DEFAULT 0"]
-                          [:graph "TEXT" "NOT NULL"]
-                          [:subject "TEXT" "NOT NULL"]
-                          [:predicate "TEXT" "NOT NULL"]
-                          [:object "TEXT" "NOT NULL"]
-                          [:datatype "TEXT" "NOT NULL"]
-                          [:annotation "TEXT"]])) 
+        statement-table-ddl (jdbc/create-table-ddl :statement
+                                                   [[:assertion :int "NOT NULL"] 
+                                                    [:retraction :int "NOT NULL DEFAULT 0"]
+                                                    [:graph "TEXT" "NOT NULL"]
+                                                    [:subject "TEXT" "NOT NULL"]
+                                                    [:predicate "TEXT" "NOT NULL"]
+                                                    [:object "TEXT" "NOT NULL"]
+                                                    [:datatype "TEXT" "NOT NULL"]
+                                                    [:annotation "TEXT"]])]
 
-  ;add tables to database
-  (jdbc/db-do-commands db-spec metadata-table-ddl)
-  (jdbc/db-do-commands db-spec prefix-table-ddl)
-  (jdbc/db-do-commands db-spec statement-table-ddl)
+    ;add tables to database
+    (jdbc/db-do-commands db-spec metadata-table-ddl)
+    (jdbc/db-do-commands db-spec prefix-table-ddl)
+    (jdbc/db-do-commands db-spec statement-table-ddl)
 
-  (jdbc/insert! db-spec :ldtab {:key "ldtab version" :value "0.0.1"})
-  (jdbc/insert! db-spec :ldtab {:key "schema version" :value "0"})) 
-
-(defn -main
-  "Currently only used for manual testing."
-  [& args]
-
-  ;expect database name as first argument
-  (create-database (first args)) 
-  ;(println (jdbc/query   db-spec ["SELECT * FROM ldtab"])) 
-  )
-
+    (jdbc/insert! db-spec :ldtab {:key "ldtab version" :value "0.0.1"})
+    (jdbc/insert! db-spec :ldtab {:key "schema version" :value "0"})))
