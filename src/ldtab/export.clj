e7ffda685b7bc55793582c31b64ff589d5dbb243
chris <chris.kind.man@gmail.com>
2022-03-29 23:00:34 +0100

    Add basic support for `export` (#5)

---
+++ src/ldtab/export.clj
@@ -0,0 +1,38 @@
+(ns ldtab.export
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.java.io :as io]
+            [clojure.string :as str])
+  (:import [java.io FileInputStream] 
+           [org.apache.jena.riot RDFDataMgr Lang])
+  (:gen-class)) 
+
+;TODO: export to a file or STDOUT
+;FORMATS: Turtle, RDFXML, HTML+RDFa, TSV table?
+
+(defn load-db
+  [path]
+  {:classname "org.sqlite.JDBC"
+  :subprotocol "sqlite"
+  :subname path})
+
+(defn triple-2-tsv
+  [triple]
+   (str/join "\t" (vals triple)))
+
+(defn get-tsv-header
+  [data]
+  (let [triple (first data)
+        ks (keys triple)
+        columns (map name ks)
+        header (str/join "\t" columns)]
+    header))
+
+(defn export-tsv
+  [db-path table output] 
+  (let [db (load-db db-path)
+        data (jdbc/query db [(str "SELECT * FROM " table " LIMIT 10")])
+        output-path (io/as-relative-path output)] 
+    (with-open [w (io/writer output-path :append true)] 
+      (.write w (str (get-tsv-header data) "\n"))
+      (doseq [row data]
+        (.write w (str (triple-2-tsv row) "\n"))))))
